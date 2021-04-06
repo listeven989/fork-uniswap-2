060cccc991cc809fba989eae5f39c4c0652ff40f
Steven Li <steven4354@gmail.com>
2021-04-05 23:03:14 -0700

    setting up initial deployment file

--- core/migrations/1_initial_migration.js
+++ core/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-const Migrations = artifacts.require("Migrations");
+const Factory = artifacts.require("UniswapV2Factory.sol");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer, _network, addresses) {
+  // deploy from the first address, the address that deployed this contract
+  deployer.deploy(Factory, addresses[0]);
+  const factory = Factory.deployed
 };
