91a96092f698732e13ced8fc5af01a091e23e043
Steven Li <steven4354@gmail.com>
2021-04-05 23:21:51 -0700

    adding token contracts to initial migration

--- core/migrations/1_initial_migration.js
+++ core/migrations/1_initial_migration.js
@@ -1,7 +1,23 @@
 const Factory = artifacts.require("UniswapV2Factory.sol");
+const Token1 = artifacts.require("Token1.sol");
+const Token2 = artifacts.require("Token2.sol");
 
 module.exports = function (deployer, _network, addresses) {
   // deploy from the first address, the address that deployed this contract
   deployer.deploy(Factory, addresses[0]);
   const factory = Factory.deployed
+
+  if (network == "mainnet") {
+    token1Address = ''
+    token2Address = ''
+  } else {
+    await deployer.deploy(Token1)
+    await deployer.deploy(Token2)
+    const token1 = Token1.deployed()
+    const token2 = Token2.deployed()
+    token1Address = token1.address
+    token2Address = token2.address
+  }
+
+  await factory.createPair(token1Address, token2Address)
 };
